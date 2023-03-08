c9ab7a675fcfa66dc195afba1875f1d94687aa35
Fabricio O. Sampaio <115957717+MINATO4K@users.noreply.github.com>
2023-03-08 05:59:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,19 +14,27 @@ echo -e "<<<<<< INSTALANDO SISTEMA E PACOTES,\nPRIORITÁRIOS PARA O BOT >>>>>";
 sleep 5
 clear;
 apt install nodejs -y;
+sleep 3;
 apt install npm -y;
+sleep 3;
 npm i -g typescript;
+sleep 3;
 npm install qrcode-terminal;
+sleep 3;
 npm i rompot;
+sleep 3;
 apt update -y;
+sleep 3;
 apt upgrade -y;
+sleep 3;
 clear; 
 
 echo "<<<<<< CRIANDO DIRETÓRIOS >>>>>";
 sleep 5
 clear
 
-mkdir dist;
 touch index.js;
+sleep 3;
 cat db.txt > index.js;
+sleep 3;
 node index.js;
